5916971daaed6c5fb67c42a0afd66fabf17129cc
Prineth Fernando <70090385+prineth@users.noreply.github.com>
2020-10-01 23:57:34 +0530

    Just upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int no,i;
+    unsigned long long int fact=1;
+    //f=factorial
+    //no=input number
+    printf("Enter any number : ");
+    scanf("%d",&no);
+
+    if(no<0)
+    {
+        printf("#Invalid number#");
+    }
+
+    else
+    {
+       for(i=0;i<=no;i++)
+        {
+            fact=f*i;
+        }
+        printf("Factorial of %d= %d",no,f);
+    }
+
+
+    return 0;
+}
